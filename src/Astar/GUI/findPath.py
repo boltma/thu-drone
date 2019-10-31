f55c42fc7f870ab6c700fa081154207aa7b1d13b
Xiaoyang Ma <boltma@gmail.com>
2019-10-31 19:18:39 +0800

    Update Astar and add comments

--- src/Astar/GUI/findPath.py
+++ src/Astar/GUI/findPath.py
@@ -1,22 +1,31 @@
+import math
+
 def findPath(mapmap, startPosition, goalPosition):
     ##########################################
     ########     initialization       ########
     ##########################################
-    direction = [(0, 1), (0, -1), (1, 0), (-1, 0), (1, 1), (1, -1), (-1, 1), (-1, -1)]  # reachable direction
+    direction = [(1, 1), (1, -1), (-1, 1), (-1, -1), (0, 1), (0, -1), (1, 0), (-1, 0)]  # reachable direction
     mapRow, mapCol = mapmap.shape
 
     ##########################################
     ########    heuristic function    ########
     ##########################################
+    coef = 2 - math.sqrt(2)
+
     def g(current, node):
-        return current[3] + abs(node[0] - current[0]) + abs(node[1] - current[1])
+        # octile distance
+        dx = abs(node[0] - current[0])
+        dy = abs(node[1] - current[1])
+        return current[3] + dx + dy - coef * min(dx, dy)
 
     def h(node, goal):
-        return abs(node[0] - goal[0]) + abs(node[1] - goal[1])
+        # octile heuristic
+        dx = abs(node[0] - goal[0])
+        dy = abs(node[1] - goal[1])
+        return dx + dy - coef * min(dx, dy)
 
-    def f(parameters):
-        # code here ...
-        pass
+    def f(g_score, h_score):
+        return g_score + h_score
 
     ##########################################
     ########     other function       ########
@@ -35,40 +44,49 @@ def findPath(mapmap, startPosition, goalPosition):
     # parentNodeIdx: index of current node's parent node in closeList
     # ----------------------------------------
     # You can change these, but remember code for visualization
-    # should also be modefied.
+    # should also be modified.
 
     # initialization
     openList = []
     closeList = []
 
-    openList.append(startPosition + (0, 0, 0, -1))
+    # put startPosition in openList
+    g_score = 0
+    h_score = h(startPosition, goalPosition)
+    openList.append(startPosition + (f(g_score, h_score), g_score, h_score, -1))
 
     while openList:
-        current = min(openList, key=lambda t: t[2])
-        openList.pop(openList.index(current))
+        # find node with smallest f and its index in openList, add it to closeList and pop from openList
+        index, current = min(enumerate(openList), key=lambda t: t[1][2])
+        openList.pop(index)
         closeList.append(current)
         currentIdx = len(closeList) - 1
+
         if current[0:2] == goalPosition:
-            break
+            break  # stop if found goalPosition
+
         for d in direction:
             neighbor = (current[0] + d[0], current[1] + d[1])
             if neighbor[0] >= mapRow or neighbor[0] < 0 or neighbor[1] >= mapCol or neighbor[1] < 0:
-                continue
+                continue  # out of range
 
             if mapmap[neighbor]:
-                continue
+                continue  # obstacle
 
             if any([node[0:2] == neighbor for node in closeList]):
-                continue
+                continue  # already in closeList
 
+            # compute g_score and h_score, add to openList
             g_score = g(current, neighbor)
             h_score = h(neighbor, goalPosition)
-            neighbor += ((g_score + h_score) * 2, g_score, h_score, currentIdx)
-            l = list(filter(lambda node: node[0:2] == neighbor[0:2], openList))
-            if not len(l):
+            neighbor += (f(g_score, h_score), g_score, h_score, currentIdx)
+            # find nodes in openList with current position
+            nodes = list(filter(lambda node: node[0:2] == neighbor[0:2], openList))
+            if not len(nodes):
                 openList.append(neighbor)
-            elif l[0][3] > g_score:
-                idx = openList.index(l[0])
+            elif nodes[0][3] > g_score:
+                # update existing nodes if g_score is smaller
+                idx = openList.index(nodes[0])
                 openList.pop(idx)
                 openList.append(neighbor)
 
